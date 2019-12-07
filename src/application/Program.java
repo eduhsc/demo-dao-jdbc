992e30fa21dd48818a730fa8ec2f5e0dd64573cf
Eduardo Haddad <eduardochaves51@gmail.com>
2019-12-07 17:16:20 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,9 +14,10 @@ public class Program {
 	public static void main(String[] args) throws ParseException {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Department obj = new Department(1, "Vendas");
-		
+		Department obj = new Department(1, "Vendas");		
 		Seller seller = new Seller(100,"John","john@gmail", new Date(sdf.parse("10/04/1990").getTime()), 3000.00,obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 	}
 
