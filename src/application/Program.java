3199436adf17444914d287c54099e5f04c4c4371
Eduardo Haddad <eduardochaves51@gmail.com>
2019-12-06 22:08:46 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Department obj = new Department(1, "Vendas");
-		System.out.println(obj.toString());
+		
+		Seller seller = new Seller(100,"John","john@gmail", new Date(sdf.parse("10/04/1990").getTime()), 3000.00,obj);
+		System.out.println(seller);
 	}
 
 }
